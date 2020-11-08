7689fdf67d0dcf2d9e6c1b6434b6051a372bf6c8
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-08 14:52:48 -0500

    botname update

--- bot.sh
+++ bot.sh
@@ -2,5 +2,5 @@
 for (( ; ; ))
 do
   R=`openssl rand -base64 12`
-  curl -d "username=alejo$R&password=123" -X POST https://recaptcha-fullstack-example.herokuapp.com/api/auth/signup
+  curl -d "username=Dougbot900$R&password=123" -X POST https://recaptcha-fullstack-example.herokuapp.com/api/auth/signup
 done
